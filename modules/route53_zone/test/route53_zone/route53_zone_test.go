7d6970b86c4fd7ce7751f4b8398dbc27f4d9109c
abhisheksr01 <abhisheksingh2691@gmail.com>
2023-09-06 11:43:37 +0100

    add test for route53 recordset values

--- modules/route53_zone/test/route53_zone/route53_zone_test.go
+++ modules/route53_zone/test/route53_zone/route53_zone_test.go
@@ -3,20 +3,20 @@ package test
 import (
 	"fmt"
 	"os"
-	// "slices"
-	// "strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAwsRoute53Zone(t *testing.T) {
+	/* ARRANGE */
 	t.Parallel()
-
 	expectedRoute53ZoneName := fmt.Sprintf("terratest.website.test.co.uk.")
 	record1 := map[string]interface{}{
 		"name":    "one",
@@ -24,7 +24,6 @@ func TestTerraformAwsRoute53Zone(t *testing.T) {
 		"ttl":     60,
 		"type":    "A",
 	}
-
 	record2 := map[string]interface{}{
 		"name":    "two",
 		"records": []string{"dummy.armakuni.co.uk"},
@@ -32,7 +31,6 @@ func TestTerraformAwsRoute53Zone(t *testing.T) {
 		"type":    "CNAME",
 	}
 	expectedRecords := []map[string]interface{}{record1, record2}
-
 	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
 	// terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -46,20 +44,18 @@ func TestTerraformAwsRoute53Zone(t *testing.T) {
 		},
 	})
 
+	/* ACTION */
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
-
 	// This will run `terraform init` and `terraform plan` and fail the test if there are any errors
 	terraform.InitAndPlan(t, terraformOptions)
-
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
+	/* ASSERTIONS */
 	// Run `terraform output` to get the value of an output variable
 	zoneID := terraform.Output(t, terraformOptions, "zone_id")
-
 	mySession := session.Must(session.NewSession())
-
 	// Create a Route53 client from just a session.
 	svc := route53.New(mySession)
 	actualHostedZone, err := svc.GetHostedZone(&route53.GetHostedZoneInput{Id: aws.String(zoneID)})
@@ -78,11 +74,22 @@ func TestTerraformAwsRoute53Zone(t *testing.T) {
 		if *b.Type == record1["type"] {
 			expectedRecordName := fmt.Sprintf("%s%s%s", record1["name"],
 				".", expectedRoute53ZoneName)
+			var actualRecordValues []string
+			for _, a := range b.ResourceRecords {
+				actualRecordValues = append(actualRecordValues, *a.Value)
+			}
+			less := func(a, b string) bool { return a < b }
+			assert.True(t, cmp.Diff(record1["records"], actualRecordValues, cmpopts.SortSlices(less)) == "")
 			assert.EqualValues(t, expectedRecordName, *b.Name)
 			assert.EqualValues(t, record1["ttl"], *b.TTL)
 		} else if *b.Type == record2["type"] {
 			expectedRecordName := fmt.Sprintf("%s%s%s", record2["name"],
 				".", expectedRoute53ZoneName)
+			var actualRecordValues []string
+			for _, a := range b.ResourceRecords {
+				actualRecordValues = append(actualRecordValues, *a.Value)
+			}
+			assert.EqualValues(t, record2["records"], actualRecordValues)
 			assert.EqualValues(t, expectedRecordName, *b.Name)
 			assert.EqualValues(t, record2["ttl"], *b.TTL)
 		} else {
@@ -95,3 +102,5 @@ func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
 }
+
+func getRecordValues()
--- modules/website_bucket/test/website_bucket/website_bucket_test.go
+++ modules/website_bucket/test/website_bucket/website_bucket_test.go
@@ -17,19 +17,16 @@ import (
 )
 
 func TestTerraformAwsS3WebsiteBucket(t *testing.T) {
+	/* ARRANGE */
 	t.Parallel()
-
 	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
 	// in your AWS account
 	expectedBucketName := fmt.Sprintf("terratest-website-bucket-test-%s", strings.ToLower(random.UniqueId()))
-
 	// AWS region set in provider.tf or versions.tf
 	expectedAwsRegion := "eu-west-2"
-
 	expectBucketPublicBlock := "{\n  PublicAccessBlockConfiguration: {\n    BlockPublicAcls: false,\n    BlockPublicPolicy: false,\n    IgnorePublicAcls: false,\n    RestrictPublicBuckets: false\n  }\n}"
 	expectedCanonicalUserPermissions := []string{"WRITE_ACP", "READ_ACP", "READ", "WRITE"}
 	expectedGroupPermissions := []string{"READ", "READ_ACP", "WRITE"}
-
 	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
 	// terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -43,29 +40,26 @@ func TestTerraformAwsS3WebsiteBucket(t *testing.T) {
 		},
 	})
 
+	/* ACTION */
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
-
 	// This will run `terraform init` and `terraform plan` and fail the test if there are any errors
 	terraform.InitAndPlan(t, terraformOptions)
-
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
+	/* ASSERTIONS */
 	// Run `terraform output` to get the value of an output variable
 	bucketID := terraform.Output(t, terraformOptions, "bucket_id")
-
 	// Verify that our Bucket has versioning enabled
 	actualStatus := awsTerratest.GetS3BucketVersioning(t, expectedAwsRegion, bucketID)
 	expectedStatus := "Enabled"
 	assert.Equal(t, expectedStatus, actualStatus)
-
 	sess, err := session.NewSession(&aws.Config{
 		Region: &expectedAwsRegion},
 	)
 	// Create S3 service client
 	svc := s3.New(sess)
-
 	//Verify that our Bucket have ACL and Permission matches the expected ones
 	actualBucketACL, err := svc.GetBucketAcl(&s3.GetBucketAclInput{Bucket: aws.String(expectedBucketName)})
 	if err != nil {
@@ -81,7 +75,6 @@ func TestTerraformAwsS3WebsiteBucket(t *testing.T) {
 			assert.True(t, slices.Contains(expectedGroupPermissions, *b.Permission))
 		}
 	}
-
 	//Verify that our Bucket is Publicly Accessible
 	actualPublicAccessBlock, err := svc.GetPublicAccessBlock(&s3.GetPublicAccessBlockInput{Bucket: aws.String(expectedBucketName)})
 	if err != nil {
