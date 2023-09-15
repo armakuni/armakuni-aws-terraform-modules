2cb6c8ecd563bb4e64423b45eb02a36060d9aeaa
Tom Oram <tom.oram@armakuni.com>
2023-09-15 17:33:40 +0100

    Remove OIDC and route53 modules
    These have been moved to their own repos:
    - https://github.com/armakuni/terraform-aws-github-actions-oidc
    - https://github.com/armakuni/terraform-aws-route53-public-hosted-zone

--- modules/route53_zone/examples/route53_zone/main.tf
+++
@@ -1,5 +0,0 @@
-module "test_route53_zone" {
-  source    = "../.."
-  zone_name = var.zone_name
-  records   = var.records
-}
--- modules/route53_zone/examples/route53_zone/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "zone_id" {
-  value = module.test_route53_zone.zone_id
-}
